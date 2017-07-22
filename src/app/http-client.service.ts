130b4150f05d1965be9bcff612d1b95fa5f7f4bb
LenaKuchko <kuchkoel@gmail.com>
2017-07-22 10:19:24 -0700

    add http-client service

---
+++ src/app/http-client.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+
+@Injectable()
+export class HttpClientService {
+
+  constructor(private http: Http) { }
+
+  createAuthorizationHeader(headers: Headers) {
+    headers.append('Authorization', 'Basic ' +
+      btoa('username:password'));
+
+    }
+
+  get(url) {
+    let headers = new Headers();
+    this.createAuthorizationHeader(headers);
+    return this.http.get(url, {
+      headers: headers
+    });
+  }
+}
