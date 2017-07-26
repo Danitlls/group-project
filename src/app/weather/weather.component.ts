2455dbb7ed579766852cc0c4e143de6dad1c80f8
Jun Fritz and Olena Kuchko <jun+olena@>
2017-07-26 14:13:16 -0700

    add weather api.

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../weather.service';
 
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
-  styleUrls: ['./weather.component.sass']
+  styleUrls: ['./weather.component.sass'],
+  providers: [ WeatherService ]
 })
 export class WeatherComponent implements OnInit {
-
-  constructor() { }
+  temp;
+  constructor(private weatherService: WeatherService) { }
 
   ngOnInit() {
+    this.weatherService.getCurrentWeather().subscribe(response => {
+      this.temp = Math.floor((1.8 * (response.json().main.temp - 273)) + 32);
+    });
   }
+
 
 }
